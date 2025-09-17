c613769b30a4928e5cadcfd10c1c733e17d9c8b4
choonsik <moodeary@gmail.com>
2025-09-17 23:43:17 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,9 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  server: {
+    port: 3001
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
